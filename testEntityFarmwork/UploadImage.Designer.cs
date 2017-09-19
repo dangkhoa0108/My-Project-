71cbbb34e3e218f47365bc0308a80767f9775a41
Luong Diep Thinh <thinhld2@smartosc.com>
2017-09-20 02:56:33 +0700

    upload images

---
+++ testEntityFarmwork/UploadImage.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace WindowsFormsApp1 {
+    partial class UploadImage {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnLoad = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(12, 226);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 0;
+            this.btnBrowse.Text = "Open";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(0, 199);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(103, 226);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 3;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(3, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(278, 174);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(197, 226);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(75, 23);
+            this.btnLoad.TabIndex = 5;
+            this.btnLoad.Text = "Load";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // UploadImage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnBrowse);
+            this.Name = "UploadImage";
+            this.Text = "Upload";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnLoad;
+    }
+}
+
---
+++ testEntityFarmwork/UploadImage.cs
@@ -0,0 +1,117 @@
+﻿//using System;
+//using System.ComponentModel;
+
+//using System.Windows.Forms;
+//using System.IO;
+//using System.Linq;
+//using System.Drawing;
+
+//namespace WindowsFormsApp1
+//{
+//    public partial class UploadImage : Form
+//    {
+        
+//        TestEntities db = new TestEntities();
+//        public UploadImage()
+//        {
+//            InitializeComponent();
+//        }
+//        private void Form1_Load(object sender, EventArgs e)
+//        {
+//            label1.Hide();
+
+//        }
+
+//        String imgsrc = "";
+//        private void btnBrowse_Click(object sender, EventArgs e)
+//        {
+//            OpenFileDialog dialog = new OpenFileDialog();
+//            dialog.Filter = dialog.Filter = "Image files (*.jpg, *.jpeg, *.jpe, *.jfif, *.png) | *.jpg; *.jpeg; *.jpe; *.jfif; *.png";
+//            if (dialog.ShowDialog() == DialogResult.OK)
+//            {
+//                var size = new FileInfo(dialog.FileName).Length;
+//                var maxBytes = 100 * 1024;
+//                if (size < maxBytes)
+//                {
+//                    label1.Show();
+//                    imgsrc = dialog.FileName.ToString();
+//                    label1.Text = dialog.FileName.ToString();
+//                    pictureBox1.ImageLocation = dialog.FileName;
+//                }
+//                else
+//                {
+
+//                    MessageBox.Show("File size: \n" + dialog.FileName + " \n> 100MB !");
+//                }
+
+
+
+
+//            }
+//        }
+
+//        private void btnSave_Click(object sender, EventArgs e)
+//        {
+//            saveImages();
+//        }
+
+//        private void btnLoad_Click(object sender, EventArgs e)
+//        {
+//            loadImages(1);
+//        }
+//        private void saveImages()
+//        {
+//            try
+//            {
+//                byte[] images = null;
+//                FileStream fs = new FileStream(imgsrc, FileMode.Open, FileAccess.Read);
+//                BinaryReader br = new BinaryReader(fs);
+//                images = br.ReadBytes((int)fs.Length);
+
+//                var img = new Test() { img = images };
+//                db.Tests.Add(img);
+
+//                db.SaveChanges();
+//                MessageBox.Show("OK");
+//            }
+//            catch (Exception ex)
+//            {
+//                MessageBox.Show(ex.Message);
+//            }
+
+//        }
+//        private void loadImages(int idUser)
+//        {
+//            try
+//            {
+
+
+//                var t = db.Tests.FirstOrDefault(i => i.id == idUser);
+//                byte[] images = t.img;
+//                //pictureBox1.ImageLocation
+//                if (images == null)
+//                {
+
+//                    pictureBox1.Image = null;
+
+//                }
+//                else
+//                {
+//                    MemoryStream ms = new MemoryStream(images);
+
+//                    var image = Image.FromStream(ms);
+//                    pictureBox1.Image = image;
+
+//                }
+//            }
+//            catch (Exception ex)
+//            {
+
+//                MessageBox.Show(ex.Message);
+//            }
+//        }
+
+       
+//    }
+//}
+
